02f9aa7d82007d5cec0b82eaebc7bdd1a39c2c0f
sahil70526 <sahilsingh70526@gmail.com>
2022-11-28 00:03:07 +0530

    todays task

--- projects/To-Do-List/index.js
+++ projects/To-Do-List/index.js
@@ -3,24 +3,35 @@ let clearBtn= document.getElementById('clear');
 let persistBtn= document.getElementById('persist');
 let val= document.getElementById("toDodata");
 let ulData= document.getElementById("toDoItems")
+let result =[];
 
-addBtn.addEventListener('click',add);
+    addBtn.addEventListener('click',add);
+    function add(){
+        // ----code for adding
+        let item= val.value;
+        result.push(item);
+       if(item){
+        let list=document.createElement('li');
+        list.innerHTML = item;
+        ulData.appendChild(list);
+        // ------------ code for removing done items ---------------------
+    
+        list.addEventListener('click',()=>{
+            list.remove();
+        });
+    }
+    val.value=' '
+       }
 
-function add(){
-    // ----code for adding
-    let item= val.value;
-    let list=document.createElement('li');
-    list.innerHTML = item;
-    ulData.appendChild(list);
-    val.value=' ';
-}
-clearBtn.addEventListener('click',clear);
-
-function clear(){
-    // ----code for removing 
-}
+        // -------------- code for removing all items-------------------
+     
+        clearBtn.addEventListener('click',()=>{
+            ulData.innerHTML=' ';
+        });
 
 persistBtn.addEventListener('click',persist);
+
+console.log(result);
 
 function persist(){
 // -----------code for persist storage---------
