bccf5cd4e0fe624b65fc00620e1c555112175e37
sahil70526 <sahilsingh70526@gmail.com>
2022-11-30 01:26:47 +0530

    tic toe started

--- projects/Tic-Tac-Toe-Game/index.js
+++ projects/Tic-Tac-Toe-Game/index.js
@@ -1,11 +1,25 @@
-let data= document.getElementById('List');
-let col=1;
+let data= document.getElementById('gamebox');
+
+let count = 0;
+let clicked= true;
 
 data.addEventListener('click',(e)=>{
-    col = col+1;
-   if(col%2){
-    e.target.style.backgroundColor='red'
-   }else{
-    e.target.style.backgroundColor='white'
-   }
-})
+    count=count+1;
+    if (count%2 && clicked) {
+        e.target.value='X'
+        e.target.style.backgroundColor='green';
+    }else{
+        e.target.value='O';
+        e.target.style.backgroundColor='pink';
+    }
+    let f1=document.getElementById('f1').value;
+    let f2=document.getElementById('f2').value;
+    let f3=document.getElementById('f3').value;
+    let f4=document.getElementById('f4').value;
+    let f5=document.getElementById('f5').value;
+    let f6=document.getElementById('f6').value;
+    let f7=document.getElementById('f7').value;
+    let f8=document.getElementById('f8').value;
+    let f9=document.getElementById('f9').value;
+    
+});
