c7828bb58258ac495dc6a965d8de0e717321254a
unknown <sahilsingh70526@gmail.com>
2022-12-06 11:19:19 +0530

    5 dec

---
+++ Daily tasks/HomeWork-5dec/Task-two/index.js
@@ -0,0 +1,12 @@
+let age= window.prompt('Enter your age');
+let underage= React.createElement(
+    'h1',null,` You are underage. you will be adult after ${18-age}`
+    
+);
+
+let normal= React.createElement(
+    'h1',null,`you are adult`
+);
+
+const Root= ReactDOM.createRoot(document.getElementById('id'));
+Root.render(age>=18?normal:underage);
