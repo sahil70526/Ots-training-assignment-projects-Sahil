7a0e4047642040767889b8a5f2b43f56d0cfb79f
unknown <sahilsingh70526@gmail.com>
2023-01-12 20:05:36 +0530

    fdsafgfgsd

--- Backend-Work/HomeWork11jan/Data.js
+++ Backend-Work/HomeWork11jan/Data.js
@@ -1,30 +1,53 @@
-const mongoose= require('mongoose')
+const mongoose = require('mongoose')
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://127.0.0.1/demo')
 
-const StudentSchema= new mongoose.Schema({
+const StudentSchema = new mongoose.Schema({
     name: String,
-    email:String,
-    roll:Number,
+    email: String,
+    roll: Number,
 });
 
-const student= mongoose.model("Student",StudentSchema);
+const student = mongoose.model("Student", StudentSchema);
 
-const db= async()=>{
-    let data= new student({
-        name:"Sahil Singh",
-        email:"sahil@.com",
-        roll:185552
-    });
-    const result = await data.save();
-    console.log(result);
-}
+// const db= async()=>{
+//     let data= new student({
+//         name:"Sahil Singh",
+//         email:"sahil@.com",
+//         roll:185552
+//     });
+//     const result = await data.save();
+//     // console.log(result);
+//     return result;
+// }
 
 // db();
 
-const dbRead= async()=>{
-let data= await student.find();
-console.log(data);
+const dbRead = async () => {
+    let data = await student.find();
+    let data1 = new student({
+        name: "Sahil Singh",
+        email: "sahil123@gmail.com",
+        roll: 185552
+    });
+    let newData = data.filter((c) => {
+        if (c.email === data1.email) {
+            return true;
+        } else {
+            return false;
+        }
+    })
+    if (newData.length === 0) {
+        await data1.save();
+    }
+    console.log(data.length);
+
+    // ---------- deleting duplicate data ------------
+
+
+
+
+
 }
 
 dbRead()
